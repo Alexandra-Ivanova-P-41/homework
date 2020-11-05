26685281a90307775393372d4841359d9ec19acf
Alexandra-Ivanova-P-41 <71428144+Alexandra-Ivanova-P-41@users.noreply.github.com>
2020-11-05 23:20:57 +0300

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-isFullName() {
-      if (this.isValid === true && this.object.match(/^[А-ЯЁ][а-яё]*(\s[А-ЯЁ][а-яё]*){2}$/)) {
-        this.isValid = true;
-        return this;
-      }
-      this.isValid = false;
-      return this;
-djsahfjhasdjfh
