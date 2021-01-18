dbb9e627e3888a62f7dd13a991ca5010090b33df
Alexandra-Ivanova-P-41 <sashaivanova1801@gmail.com>
2021-01-19 01:16:37 +0300

    fix issues

---
+++ index.js
@@ -0,0 +1,191 @@
+var authUserData = null;
+var userDatabase = [];
+
+function isEmailCorrect(email) {
+  if (email.match(/^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i) !== null) {
+    return true;
+  }
+  return false;
+}
+
+function isPasswordCorrect(password) {
+  if (password.length === 6 && password[0] === password[0].toUpperCase() && /[0-9]/g.test(password)) {
+    return true;
+  }
+  return false;
+}
+
+
+function resetPassword(email, oldPassword, newPassword) {
+  for (let i = 0; i < userDatabase.length; i++) {
+    if (userDatabase[i].email === email && oldPassword !== newPassword && isPasswordCorrect(newPassword)) {
+      userDatabase[i].password = newPassword;
+    }
+  }
+  return false;
+}
+
+function register(email, password) {
+  let check = null;
+  if (isEmailCorrect(email) && isPasswordCorrect(password)) {
+    check = userDatabase.findIndex((user) => user.email === email && user.password === password);
+    if (check === -1) {
+      userDatabase.push({ email: email, password: password });
+      return true;
+    }
+  }
+  return false;
+}
+
+function signIn(email, password) {
+  let check = null;
+  if (isEmailCorrect(email) && isPasswordCorrect(password)) {
+    check = userDatabase.findIndex((user) => user.email === email && user.password === password);
+    if (check === 1) {
+      authUserData = true;
+      return true;
+    }
+  }
+  return false;
+}
+
+function signOut() {
+  authUserData = null;
+}
+
+
+function isAuth() {
+  if (authUserData) {
+    return true;
+  }
+  return false;
+}
+
+function validator(value) {
+  return {
+    object: value,
+    isValid: true,
+    validate: function() {
+      return this.isValid;
+    },
+
+    max(number) {
+      if (this.isValid && this.object <= number) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return false;
+    },
+
+    min(number) {
+      if (this.isValid && this.object > number) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return false;
+    },
+
+    maxLenght(array) {
+      if (this.isValid && this.object.lenght <= array.lenght) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return false;
+    },
+
+    minLenght(array) {
+      if (this.isValid && this.object.lenght > array.lenght) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return false;
+    },
+
+    isEmail() {
+      if (this.isValid && this.object.match(/^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i) !== null) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    },
+
+    isString() {
+      if (this.isValid && this.object instanceof String) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return false;
+    },
+
+    equal(check) {
+      if (this.isValid && check.toString() === this.object.toString()) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    },
+
+    isArray() {
+      if (this.isValid && this.object instanceof Array) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    },
+
+    isNumber() {
+      if (this.isValid && this.object instanceof Number) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    },
+
+    isFloat() {
+      if (this.isValid && !isNaN(this.object) && this.object.toString().indexOf('.') !== -1) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    },
+
+    isDate() {
+      if (this.isValid && this.object.match(/^\d{1,2}\.\d{1,2}\.\d{4}$/) !== null) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    },
+
+    isRussianPhone() {
+      if (this.isValid && this.object.match(/^[+]*[(]{0,1}[7-8]{1,3}[)]{0,1}[-\s\./0-9]*$/g) !== null) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    },
+
+    isFullName() {
+      if (this.isValid && this.object.match(/^[А-ЯЁ][а-яё]*(\s[А-ЯЁ][а-яё]*){2}$/)) {
+        this.isValid = true;
+        return this;
+      }
+      this.isValid = false;
+      return this;
+    }
+  }
+}
+
+console.log(validator("Иванов Иван").isFullName().validate());
